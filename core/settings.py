d42c2d88ec7705ba29d53c88fc02e853f2f4a366
Pmabouba27 <maboubapriscille@gmail.com>
2026-05-09 18:09:48 +0100

    Fix: ALLOWED_HOSTS pour lab-devops-self.vercel.app

--- core/settings.py
+++ core/settings.py
@@ -36,8 +36,13 @@ SECRET_KEY = 'django-insecure-3qgyi1s0c_%elxe%0fn!+_m6_2+go9=il2%&^pgd9*))h$lv$m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lab-devops-4oa7.vercel.app',]
-
+ALLOWED_HOSTS = ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'lab-devops-self.vercel.app',
+    'lab-devops-4oa7.vercel.app',
+    'lab-devops-4mhe.vercel.app',
+]
 # Application definition
 
 INSTALLED_APPS = [
